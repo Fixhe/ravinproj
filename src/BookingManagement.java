ca92b26b6405b68ae0d3a6834019ee5624a82b9c
Fixhe <ravinmengheang830@gmail.com>
2024-02-08 16:39:37 +0700

    Second Test

---
+++ src/BookingManagement.java
@@ -0,0 +1,15 @@
+
+public class BookingManagement {
+    private String CusName;
+    public BookingManagement(){
+
+    }
+
+    public BookingManagement(String cusName) {
+        CusName = cusName;
+    }
+    public void output(){
+        System.out.println("Name");
+    }
+
+}
