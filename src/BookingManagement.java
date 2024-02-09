9c9b6a678bf3de1f6dc8df1ce91ea5c6f656e987
Fixhe <ravinmengheang830@gmail.com>
2024-02-09 17:14:28 +0700

    delete

--- src/BookingManagement.java
+++ src/BookingManagement.java
@@ -1,15 +1,170 @@
 
-public class BookingManagement {
-    private String CusName;
-    public BookingManagement(){
+import java.sql.SQLOutput;
+import java.util.Scanner;
 
+public class BookingManagement {
+    Scanner input=new Scanner(System.in);
+    public String CusName;
+    private String CusPhone;
+    private String CheckDate;
+    private String DepartDate;
+    public BookingManagement(){
+    this.CusName="Ravin";
+    this.CusPhone="0998877666";
+    this.CheckDate="2024-02-20";
+    this.DepartDate="2024-02-21";
+    }
+    public BookingManagement(String CusName, String CusPhone, String CheckDate, String DepartDate){
+            this.CusName=CusName;
+            this.CusPhone=CusPhone;
+            this.CheckDate=CheckDate;
+            this.DepartDate=DepartDate;
+    }
+    public String getCusName() {
+        return CusName;
     }
 
-    public BookingManagement(String cusName) {
+    public String getCusPhone() {
+        return CusPhone;
+    }
+
+    public String getCheckDate() {
+        return CheckDate;
+    }
+
+    public String getDepartDate() {
+        return DepartDate;
+    }
+
+    public void setCusName(String cusName) {
         CusName = cusName;
     }
+
+    public void setCusPhone(String cusPhone) {
+        CusPhone = cusPhone;
+    }
+
+    public void setCheckDate(String checkDate) {
+        CheckDate = checkDate;
+    }
+
+    public void setDepartDate(String departDate) {
+        DepartDate = departDate;
+    }
+
+    //Input Information;
+    public void input(){
+        int n=0;
+        System.out.print("Enter CustomerName:");
+        CusName=input.next();
+        System.out.print("Enter CustomerPhone:");
+        CusPhone=input.next();
+        System.out.print("Enter CheckingDate:");
+        CheckDate=input.next();
+        System.out.print("Enter DepartureDate:");
+        DepartDate=input.next();
+
+        System.out.println("------------------------");
+    }
+    //Report of all Booking
     public void output(){
-        System.out.println("Name");
+        System.out.printf("|%-15s|%-20s|%-15s|%-15s|%n",getCusName(),getCusPhone(),getCheckDate(),getDepartDate());
+        System.out.printf("|____________________________________________________________________|%n");
+    }
+    //manu1;
+    public void manu1(){
+        System.out.println("===============Booking Management============");
+        System.out.println("1.Booking Management");
+        System.out.println("2.Room    Management");
+        System.out.println("3.Report of all Booking");
+        System.out.println("4.Report of all Rooms");
+        System.out.println("0.Exit...");
+    }
+    //manu booking management;
+    public void manu2(){
+        System.out.println("1.Add  Booking Information");
+        System.out.println("2.Searching of Booking Information");
+        System.out.println("3.Edit Booking Information");
+        System.out.println("4.Listing of Booking Type");
+        System.out.println("5.Deleting the Booking type");
+        System.out.println("0.Back To Home Page");
+    }
+    //LoginSystem
+    public void loginSystem(){
+        String username="dre11";
+        String password="pass11";
+        System.out.print("Enter Username:");
+        String enterusername=input.next();
+        System.out.print("Enter Password:");
+        String enterpassword=input.next();
+        if(enterusername.equals(username) && enterpassword.equals(password)){
+            System.out.println("Login Success..");
+        }else {
+            System.out.println("Fail..");
+            System.exit(0);
+        }
+    }
+    public void Search(BookingManagement[] bookings, int n){
+        String SCusphone;
+        System.out.print("Input CustomerPhone to search:");
+        SCusphone=input.next();
+        boolean found=false;
+        for(int i=0;i<n;i++){
+            if(bookings[i].getCusPhone().equals(SCusphone)){
+                found=true;
+                bookings[i].output();
+                System.out.println("Search was found...");
+            }
+        }if (found=false){
+            System.out.println("CustomerPhone not found....");
+        }
+
+    }
+    public void  Update(BookingManagement[] bookings, int n) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input CustomerPhone to edit:");
+        String upPhone = in.next();
+        String upCusname;
+        String upCuscheckdate;
+        String upCusdepartdate;
+        int b = 0;
+        for (int i = 0; i < n; i++) {
+            if (bookings[i].getCusPhone().equals(upPhone)) {
+                b = 1;
+                System.out.print("Input New CustomerName        :");
+                upCusname = in.next();
+                System.out.print("Input New CheckingDate    :");
+                upCuscheckdate = in.next();
+                System.out.print("Input New DapartureDate  :");
+                upCusdepartdate = in.next();
+
+                bookings[i].setCusName(upCusname);
+                bookings[i].setCheckDate(upCuscheckdate);
+                bookings[i].setDepartDate(upCusdepartdate);
+            }
+        }
+        if (b == 0) {
+            System.out.println("invalid room id");
+        }
+    }
+    public void Delete(BookingManagement[] Bookings,int n){
+        String DeCusPhone;
+        int b=0;
+        System.out.println("Input CustomerPhone to Delete:");
+        DeCusPhone=input.next();
+        for(int i=0;i<n;i++){
+            if (Bookings[i].getCusPhone().equals(DeCusPhone)){
+                for(int j=i;j<n-1;j++){
+                    Bookings[j]=Bookings[j+1];
+                }
+                n=n-1;
+                b=1;
+            }
+            System.out.println("Delete was completed...");
+        }
+        if(b==0){
+            System.out.println("Delete wad failed...");
+        }
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,97 @@
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
-        BookingManagement bookings=new BookingManagement();
-        System.out.println("test,...");
-        bookings.output();
+        Scanner input=new Scanner(System.in);
+        int n=0,op1,op2;
+        String st1,st2;
+        boolean exit=false;
+        BookingManagement b1=new BookingManagement();//for manu
+        //b1.loginSystem();
+        BookingManagement[] bookings=new BookingManagement[100000];
+        do{
+            b1.manu1();
+            System.out.print("which one do you want choose:");
+            op1=input.nextInt();
+            switch (op1){
+                //Booking Management;
+                case 1:{
+                    do{
+                        b1.manu2();
+                        System.out.print("Pls Choose One:");
+                        op2=input.nextInt();
+                        switch(op2){
+                            //add booking information;
+                            case 1:{
+                                System.out.print("Input N:");
+                                n=input.nextInt();
+                                int i;
+                                for(i=0;i<n;i++){
+                                    bookings[i]=new BookingManagement();
+                                    bookings[i].input();
+                                }
+                                System.out.print("Enter [any key] To Continues...");
+                            }break;
+                            //edit booking information;
+                            case 2:{
+                                BookingManagement b= new BookingManagement();
+                                System.out.printf("|--------------------------------------------------------------------|%n");
+                                b.Search(bookings,n);
+                            }break;
+                            case 3:{
+                                BookingManagement b = new BookingManagement();
+                                b.Update(bookings,n);
+//                                   Update(bookings,n);
+                                System.out.print("Enter [any key] To Continues...");
+                            }break;
+                            //listing of booking type;
+                            case 4:{
+
+                                System.out.print("Enter [any key] To Continues...");
+                            }break;
+                            //deleting the booking type;
+                            case 5:{
+                                BookingManagement b=new BookingManagement();
+                                b.Delete(bookings,n);
+
+                                System.out.print("Enter [any key] To Continues...");
+                            }break;
+                            case 0 :{
+                                System.out.println("Typing exit:");
+
+                            }break;
+                            default:{
+                                System.out.println("Invalid choice...");
+                            }
+                        }
+                        st1=input.next();
+                    }while(!st1.equals("exit") );
+                }break;
+                //Room Management;
+                case 2:{
+
+                }break;
+                //Report of all Booking;
+                case 3:{
+                    System.out.printf("|--------------------------------------------------------------------|%n");
+                    System.out.printf("|%-15s|%-20s|%-15s|%-15s|%n","CustomerName","CustomerPhone","ChekingDate","DepartuerDate");
+                    System.out.printf("|____________________________________________________________________|%n");
+                    for (int i=0;i<n;i++){
+                        bookings[i].output();
+                    }
+                }break;
+                //Report of all Rooms
+                case 4:{
+
+                }break;
+                case 0:{
+                    System.exit(0);
+                }
+            }
+            System.out.println(" ");
+            System.out.print("press [any key] to continues:");
+            st2=input.next();
+        }while(!st2.equals("exit"));
     }
 }
